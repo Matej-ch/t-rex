e306755499d92e29d11915fe957611f4f243a61b
Matej <matejchalachan@gmail.com>
2022-10-16 13:51:48 +0200

    textures and few updates added

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded',() => {
 
     const grid = document.querySelector('.grid');
 
+    const body = document.querySelector('body')
+
+    const alert = document.getElementById('alert')
+
     let isJumping = false;
 
     let gravity = 0.9;
@@ -24,32 +28,29 @@ document.addEventListener('DOMContentLoaded',() => {
     let position = 0;
 
     function jump() {
-        let count = 0;
-        let position = 0;
-        let timerId = setInterval(function() {
-
-            //down
-            if(count === 15 ) {
-                clearInterval(timerId);
+        let count = 0
+        let timerId = setInterval(function () {
+            //move down
+            if (count === 15) {
+                clearInterval(timerId)
                 let downTimerId = setInterval(function () {
-                    if(count === 0) {
-                        clearInterval(downTimerId);
-                        isJumping = false;
+                    if (count === 0) {
+                        clearInterval(downTimerId)
+                        isJumping = false
                     }
-                    position -= 5;
-                    count--;
-                    position = position * gravity;
-                    dino.style.bottom = `${position}px`;
-                },20);
+                    position -= 5
+                    count--
+                    position = position * gravity
+                    dino.style.bottom = position + 'px'
+                },20)
 
             }
-
-            position += 30;
-            count++;
-            position = position * gravity;
-            dino.style.bottom = `${position}px`;
-
-        },20);
+            //move up
+            position +=30
+            count++
+            position = position * gravity
+            dino.style.bottom = position + 'px'
+        },20)
     }
 
     function generateObstacles() {
@@ -66,21 +67,22 @@ document.addEventListener('DOMContentLoaded',() => {
         let timerId = setInterval(function () {
             if(obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
                 clearInterval(timerId);
+                alert.innerHTML = 'Game Over'
                 isGameOver = true;
 
                 ///remove children
-
+                body.removeChild(body.firstChild)
                 while (grid.firstChild) {
                     grid.removeChild(grid.lastChild);
                 }
-                grid.appendChild(dino);
+                //grid.appendChild(dino);
             }
             obstaclePosition -= 10;
             obstacle.style.left = `${obstaclePosition}px`;
         },20);
 
         if(!isGameOver) {
-            setTimeout(generateObstacles,500);
+            setTimeout(generateObstacles,randomTime);
         }
     }
 
